b94fa44c1e97b3a2f8131b015689f03e99e42df7
SamroodAli <samroodali@live.com>
2021-01-05 12:01:08 +0530

    Add:Timer on tomato

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
+TIMER_FONT = (FONT_NAME, 25, "bold")
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
@@ -30,6 +31,8 @@ tomato_img = PhotoImage(file="tomato.png")
 # for center image, give x and y half of width and height, x = 101 not 100 as the image was due to some reason
 # shifted to the right
 canvas.create_image(101, 112, image=tomato_img)
+# laying text on top, x and y placements 103,130, fill = font-color, font-tuple
+canvas.create_text(103, 130, text="00:00", fill="white", font=TIMER_FONT)
 canvas.pack()
 
 window.mainloop()
